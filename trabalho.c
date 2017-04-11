29d31fb5b3c51d0527bd1b43d247bb6294324031
up201404302 <up201404302@fe.up.pt>
2017-04-11 17:39:20 +0100

    TRABALHO

--- trabalho.c
+++ trabalho.c
@@ -8,11 +8,24 @@
 #include <sys/wait.h>
 #include <wait.h>
 #include <stdlib.h>
+#include <signal.h> 
 
 
-void scan_dir(char* c);
+void sigint_handler(int signo) 
+{ 
+  
+  char c;
+  printf("ARE YOU SURE? (Y/N)\n"); 
+  scanf("%c",&c);
+  if(c == 'Y' || c == 'y') {
+	kill(-getpgrp(),SIGUSR1);
+  }
+	 
+} 
 
-void checkfordir(char* c, int* pid) {
+void scan_dir(char* c, char* p, int l);
+
+void checkfordir(char* c, int* pid, char * p, int l) {
  DIR *d;
   d = opendir(c);
   struct dirent *dir;
@@ -36,23 +49,29 @@ void checkfordir(char* c, int* pid) {
          if( s.st_mode & S_IFDIR )
          {
             //it's a directory
-	    printf("FORK\n");
+
 	    pid[b] = fork();
     	    b++;
-	    scan_dir(v);
+	    if(pid[b-1] <= 0) {
+		 struct sigaction action; 
+ 		 action.sa_handler = SIG_IGN; 
+		 sigemptyset(&action.sa_mask); 
+		 action.sa_flags = 0; 
+		 sigaction(SIGINT,&action,NULL);
+		 scan_dir(v,p,l);
+	    }
 	 }
       }
     }
   }
+  closedir(d);
   return;
 }
 
-void checkforfiles(char* c) {
- DIR *d;
+void checkforfiles(char* c, char *p, int l) {
+  DIR *d;
   d = opendir(c);
   struct dirent *dir;
-
-      printf("%s\n",c);
 
   if (d)
   {
@@ -74,25 +93,34 @@ void checkforfiles(char* c) {
          if(!(s.st_mode & S_IFDIR) )
          {
             //it's a directory
-	    printf("FILE\n");
-	    printf("%s\n",dir->d_name);
+	    if(strcmp(dir->d_name, p) == 0) {
+	   	if(l == 1) 
+		   printf("%s\n",v);
+	        else if(l == 0) {
+		   int i = remove(v);
+		   if(i == 0)
+			printf("%s\n",p);
+		   else
+			printf("COULDNt REMOVE\n");		
+		}
+	    }
 	 }
       }
     }
   }
-
+  closedir(d);
 }
 
-void scan_dir(char* c) {
+void scan_dir(char* c, char *p, int l) {
   int pid[100], status;
-  checkfordir(c,pid);
-  checkforfiles(c);
+  checkfordir(c,pid,p,l);
+  sleep(30);
+  checkforfiles(c,p,l);
   for (int i = 0; i < 100; i++) {
     if(pid[i] > 0) {
 	waitpid(pid[i],&status,WNOHANG);
     }
     else {
-	printf("EXITING\n");
 	exit(0);
     }
   }
@@ -100,10 +128,23 @@ void scan_dir(char* c) {
 }
 
 
-int main(void)
+int main(int argc, char *argv[])
 {
+ struct sigaction action; 
+ action.sa_handler = sigint_handler; 
+ sigemptyset(&action.sa_mask); 
+ action.sa_flags = 0; 
+ if (sigaction(SIGINT,&action,NULL) < 0) 
+  { 
+    fprintf(stderr,"Unable to install SIGINT handler\n"); 
+    exit(1); 
+  } 
+if (strcmp(argv[1], "-name") == 0 && strcmp(argv[3], "-delete"))
+  scan_dir("/home/jsaraiva/Desktop", argv[2],1);
 
-  scan_dir("/home/jsaraiva/Desktop");
+else if (strcmp(argv[1], "-name") == 0 && strcmp(argv[3], "-print"))
+  scan_dir("/home/jsaraiva/Desktop", argv[2],0);
+
 
   return(0);
 }
