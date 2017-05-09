3b51f356c3722ef901f6962f705f8ba2e646bc67
up201404302 <up201404302@fe.up.pt>
2017-05-09 16:17:54 +0100

    Update sauna.c

--- TRABALHOFINAL/sauna.c
+++ TRABALHOFINAL/sauna.c
@@ -1,4 +1,4 @@
-#include <stdio.h> 
+ #include <stdio.h> 
 #include <unistd.h> 
 #include <sys/types.h> 
 #include <termios.h> 
@@ -29,7 +29,7 @@ struct PEDIDOEFIFO {
 
   struct Pedidos pedido;
   int fifo;
-}pedidoefifo;
+} pedidoefifo;
 
 pthread_mutex_t mut=PTHREAD_MUTEX_INITIALIZER;  // mutex p/a sec.critica 
 
@@ -39,15 +39,6 @@ int ocupados = 0;
 char genero = 'N';
 clock_t start, end;
 int file;
-
-int readline(int fd, char *str) 
-{ 
-  int n; 
-  do { 
-    n = read(fd,str,1); 
-  } while (n>0 && *str++ != '\0'); 
-  return (n>0); 
-} 
 
 void write_pedido(struct Pedidos pedido, int type)
 {
@@ -82,58 +73,54 @@ void * thrpedido_entrada(void * arg)
   while(1) {
 
   //Sauna Vazia
-	  if(genero == 'N') {
-    	    pthread_mutex_lock(&mut);
-	    genero = pedido.genero;
-	    printf("Pedido %d a entrar %f\n",pedido.n_pedido,pedido.tempo_util);
-	    ocupados++;
-	    pthread_mutex_unlock(&mut); 
-	    sleep(pedido.tempo_util);
-	    pthread_mutex_lock(&mut);
-	    ocupados--;
-	    if(ocupados == 0)
-		genero = 'N';
-	    printf("Pedido %d a sair\n",pedido.n_pedido);
-  	    write_pedido(pedido,2);
-	    pthread_mutex_unlock(&mut); 
+    if(genero == 'N') {
+      pthread_mutex_lock(&mut);
+      genero = pedido.genero;
+      printf("Pedido %d a entrar %f\n",pedido.n_pedido,pedido.tempo_util);
+      ocupados++;
+      pthread_mutex_unlock(&mut); 
+      sleep(pedido.tempo_util);
+      pthread_mutex_lock(&mut);
+      ocupados--;
+      if(ocupados == 0)
+	genero = 'N';
+      printf("Pedido %d a sair\n",pedido.n_pedido);
+      write_pedido(pedido,2);
+      pthread_mutex_unlock(&mut); 
 
-		return NULL;
-	  }
+      return NULL;
+    }
 
 	  //Pedido do genero igual ao da sauna
-	  else if(genero == pedido.genero) {
+    else if(genero == pedido.genero) {
 	    		//Sauna não cheia
-		    if(ocupados < n_lugares) {
-		      printf("Pedido %d a entrar %f\n",pedido.n_pedido,pedido.tempo_util);
-		      pthread_mutex_lock(&mut);
-		      ocupados++;
-		      pthread_mutex_unlock(&mut); 
-		      sleep(pedido.tempo_util);
-		      pthread_mutex_lock(&mut);
-		      ocupados--;
-		      printf("Pedido %d a sair\n",pedido.n_pedido);
-	    	      if(ocupados == 0)
-			genero = 'N';
-                      write_pedido(pedido,2);
-		      pthread_mutex_unlock(&mut); 
+      if(ocupados < n_lugares) {
+        printf("Pedido %d a entrar %f\n",pedido.n_pedido,pedido.tempo_util);
+	pthread_mutex_lock(&mut);
+	ocupados++;
+	pthread_mutex_unlock(&mut); 
+	sleep(pedido.tempo_util);
+	pthread_mutex_lock(&mut);
+	ocupados--;
+	printf("Pedido %d a sair\n",pedido.n_pedido);
+	if(ocupados == 0)
+	  genero = 'N';
+        write_pedido(pedido,2);
+	pthread_mutex_unlock(&mut); 
 
+        return NULL; 
+      }
 
+    }
 
-
-		      return NULL; 
-		    }
-
-	  }
-
-	  //Pedido do genero diferente ao da sauna
-	  else {
-	    printf("Pedido %d rejeitado\n",pedido.n_pedido);
-	    pedido.n_rejeit++;
-            write(frejeitados,&pedido,sizeof(pedido));
-            write_pedido(pedido,1);
-	    return NULL; 
-
-	  }
+    //Pedido do genero diferente ao da sauna
+    else {
+      printf("Pedido %d rejeitado\n",pedido.n_pedido);
+      pedido.n_rejeit++;
+      write(frejeitados,&pedido,sizeof(pedido));
+      write_pedido(pedido,1);
+      return NULL; 
+    }
 
   }
   return NULL;
@@ -153,12 +140,12 @@ void * thrpedido_receber(void * arg)
   printf("PEDIDOS RECEBIDOS:\n");
 
   while(read(fentrada,&pedidos,sizeof(pedidos)) > 0) {
-   printf("%d %c % f\n", pedidos.n_pedido, pedidos.genero, pedidos.tempo_util);
+    printf("%d %c % f\n", pedidos.n_pedido, pedidos.genero, pedidos.tempo_util);
    
-   pedfifo[i].pedido = pedidos;
-   pedfifo[i].fifo = frejeitados;
-   pthread_create(&tpedidos[i], NULL, thrpedido_entrada, (void *) &pedfifo[i]); 
-   i++;
+    pedfifo[i].pedido = pedidos;
+    pedfifo[i].fifo = frejeitados;
+    pthread_create(&tpedidos[i], NULL, thrpedido_entrada, (void *) &pedfifo[i]); 
+    i++;
   }
 
 
@@ -180,46 +167,43 @@ int main(int argc, char* argv[])
   int pid = getpid();
   snprintf(filename, sizeof(filename), "/tmp/bal.%d.txt", pid);
   file = open(filename, O_WRONLY | O_CREAT | O_EXCL, 0644); 
- //Check Numero de argumentos
- if(argc != 2) {
-  fprintf(stderr, "Usage: %s dir_name\n", argv[0]);
-  exit(1);
- }
+  //Check Numero de argumentos
+  if(argc != 2) {
+    fprintf(stderr, "Usage: %s dir_name\n", argv[0]);
+    exit(1);
+  }
 
- int   fentrada, frejeitados; 
- pthread_t tpedidos_recebidos; 
+  int   fentrada, frejeitados; 
+  pthread_t tpedidos_recebidos; 
 
+  //Criar Fifo da entrada
+  fentrada=open("/tmp/entrada",O_RDONLY);
 
+  //Check se abriu o fifo de entrada
+  if(fentrada == -1) {
+    perror("Can't Open Entrada Fifo\n");
+    exit(2);
+  } 
 
- //Criar Fifo da entrada
- fentrada=open("/tmp/entrada",O_RDONLY);
+  //Criar fifo dos rejeitados
+  frejeitados=open("/tmp/rejeitados",O_WRONLY);
 
- //Check se abriu o fifo de entrada
- if(fentrada == -1) {
-   perror("Can't Open Entrada Fifo\n");
-   exit(2);
- } 
-
- //Criar fifo dos rejeitados
- frejeitados=open("/tmp/rejeitados",O_WRONLY);
-
-
- //Check se abriu o fifo dos rejeitados
- if(frejeitados == -1) {
-   perror("Can't Open Rejeitados Fifo\n");
-   exit(3);
- } 
+  //Check se abriu o fifo dos rejeitados
+  if(frejeitados == -1) {
+    perror("Can't Open Rejeitados Fifo\n");
+    exit(3);
+  } 
  
- n_lugares = atoi(argv[1]);
- struct FIFOS fifos;
- fifos.fentrada = fentrada;
- fifos.frejeitados = frejeitados;
- pthread_create(&tpedidos_recebidos, NULL, thrpedido_receber, (void *) &fifos); 
+  n_lugares = atoi(argv[1]);
+  struct FIFOS fifos;
+  fifos.fentrada = fentrada;
+  fifos.frejeitados = frejeitados;
+  pthread_create(&tpedidos_recebidos, NULL, thrpedido_receber, (void *) &fifos); 
 
- pthread_join(tpedidos_recebidos, NULL); 
- close(file);
- close(fentrada); 
- close(frejeitados); 
+  pthread_join(tpedidos_recebidos, NULL); 
+  close(file);
+  close(fentrada); 
+  close(frejeitados); 
 
- return 0; 
+  return 0; 
 } 
